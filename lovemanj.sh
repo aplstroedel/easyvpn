a73c7049a6377fd579b845ca543984331bf69615
aplstroedel <kevindeleebeeck@pandora.be>
2016-12-01 06:55:42 +0100

    something new

---
+++ lovemanj.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+done=0
+
+while true ; do
+echo "					I"
+sleep 0.5
+echo "					♥"
+sleep 0.5
+echo "				     Manjaro"
+sleep 0.5
+    if [ something ]; then
+        break
+    fi
+done
