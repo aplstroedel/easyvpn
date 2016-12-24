f36e782aff2e272040daaf80c1ebfb05283dff4a
aplstroedel <kevindeleebeeck@pandora.be>
2016-12-24 18:12:44 +0100

    updated 18:12 zaterdag 24 december 24-12-16

---
+++ breakoutwhile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+done=0
+
+while true ; do
+clear 
+#echo "test1" | toilet -f term --gay | pv -qL 25
+#sleep 0.5 
+#echo "test2" | toilet -f term --gay | pv -qL 25
+#sleep 0.5
+#echo "test3" | toilet -f term --gay | pv -qL 25
+#sleep 0.5
+echo -ne 'today = ' | toilet -f term --gay | pv -qL 25   ???     date +%R\ %A\ %e\ %B\ %x | toilet -f term --gay | pv -qL 25
+    if [ something ]; then
+        break
+    fi
+done
