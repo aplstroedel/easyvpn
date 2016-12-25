e7d5a60792b7ff965f2ada985553405910fc64d2
aplstroedel <kevindeleebeeck@pandora.be>
2016-12-25 07:17:26 +0100

    updated 07:17 zondag 25 december 25-12-16

--- colorhellodate.sh
+++ colorhellodate.sh
@@ -1,20 +1,38 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 ######################################
-#A typwriter that welcoms you        #
+#   typwriterfun that welcoms you    #
 #and writes the current date notation#
-#in the format that's been used      #
+#   in the format that's been used   #
 #                                    #
-#PS: pv and toilet as dependencies   #
+#ncat, pv and toilet as dependencies #
+#                                    #
+# automated netcat server = line 24  #
+#    keeps listening until ctrl+c    #
+#                                    #
+#               USAGE:               #
+#    path2=/your/directory/here      #
+#                                    #
+#        from remote machine:        #
+#          ncat <ip> <port>          #
 ######################################
 
-done=0
+location='/$HOME'
+path2='/git/testffs'			# <-- directory of this script
+
+
+ncat -v -lk -p 6000 --send-only -c "source /$location/$path2/colorhellodate.sh; fun"
+
+fun(){
+
 while true ; do
 clear 
 printf "Good day :)" | toilet -f term --gay | pv -qL 125
 sleep 0.30
-printf 'today = %s\n' "$(date +%R\ %A\ %e\ %B\ %x)"  | toilet -f term --gay | pv -qL 125
+printf 'today = %s\n' "$(date +%R\ %A\ %e\ %B\ %d-%m-%Y)"  | toilet -f term --gay | pv -qL 75
+sleep 0.30
     if [ something ]; then
         break
     fi
 done
+}
