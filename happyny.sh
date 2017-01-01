677015bfd41a927a549396fc40eaf6fae9919669
aplstroedel <kevindeleebeeck@pandora.be>
2017-01-01 21:59:34 +0100

    updated 21:59 zondag  1 januari 01-01-17

--- happyny.sh
+++ happyny.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 hpny(){
 clear
-NY=('Happy ' 'New ' 'Year ')
 for i in "$@"
 do
-printf '%s\n' "${i}" | figlet | toilet --irc -f term -d /usr/share/figlet/fonts/ --gay | pv -qL 250
+printf '%s' "${i}" | figlet | toilet --irc --gay | pv -qL 250
 done
 }
 hpny "$@"
